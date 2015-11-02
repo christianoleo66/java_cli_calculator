9bffca70b72e0a7a64728d44c1634b7b9b02f09a
Thorben Egberts <thorben.egberts@plentymarkets.com>
2015-11-02 10:45:34 +0100

    add basic files

---
+++ src/main/java/com/plentymarkets/cli_calculator/App.java
@@ -0,0 +1,13 @@
+package com.plentymarkets.cli_calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
